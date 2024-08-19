86a48b042da2aefdd75a00091908e1ae68425ecf
Andrew Tan <andtan91@gmail.com>
2024-08-19 18:10:20 -0400

    Ignore to fix compilation issue.

--- dynamic-amm-quote/src/depeg/marinade.rs
+++ dynamic-amm-quote/src/depeg/marinade.rs
@@ -4,13 +4,14 @@ use prog_dynamic_amm::constants::depeg;
 use std::convert::TryInto;
 
 pub fn get_virtual_price(bytes: &[u8]) -> Option<u64> {
-    let stake_state = State::deserialize(&mut &bytes[8..]).ok()?;
+    unimplemented!();
+    // let stake_state = State::deserialize(&mut &bytes[8..]).ok()?;
 
-    let virtual_price = (stake_state.msol_price as u128)
-        .checked_mul(depeg::PRECISION as u128)?
-        .checked_div(State::PRICE_DENOMINATOR as u128)?;
+    // let virtual_price = (stake_state.msol_price as u128)
+    //     .checked_mul(depeg::PRECISION as u128)?
+    //     .checked_div(State::PRICE_DENOMINATOR as u128)?;
 
-    virtual_price.try_into().ok()
+    // virtual_price.try_into().ok()
 }
 
 pub mod stake {
